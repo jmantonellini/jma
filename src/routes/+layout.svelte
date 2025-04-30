c3475f9580bdd611662ea132bec9f1d2141c7b79
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2025-04-29 20:19:08 -0500

    Added Umami for analytics and referral link to DO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-import type { Snippet } from 'svelte';
-import { i18n } from '$lib/i18n';
-import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import type { Snippet } from 'svelte';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 
-import ViewTransition from './navigation.svelte';
-import Header from '$lib/ui/header/NavBar.svelte';
-import Toasts from '$lib/ui/toasts/Toasts.svelte';
-import { setToastState } from '$states/toast.svelte';
-import '../app.css';
-import { fly } from 'svelte/transition';
+	import UmamiAnalytics from '$lib/ui/custom/UmamiAnalytics.svelte';
+	import ViewTransition from './navigation.svelte';
+	import Header from '$lib/ui/header/NavBar.svelte';
+	import Toasts from '$lib/ui/toasts/Toasts.svelte';
+	import { setToastState } from '$states/toast.svelte';
+	import '../app.css';
+	import { fly } from 'svelte/transition';
 
-let { children, url }: { children: Snippet; url: string } = $props();
+	let { children, url }: { children: Snippet; url: string } = $props();
 
-setToastState();
+	setToastState();
 </script>
 
-<ParaglideJS i18n={i18n}>
+<UmamiAnalytics />
+
+<ParaglideJS {i18n}>
 	<ViewTransition />
 	<Toasts />
 	<div class="box-border font-jost grid h-full min-h-[100vh] w-full grid-rows-[auto_1fr_auto]">
@@ -30,46 +33,53 @@ setToastState();
 				{@render children()}
 			</main>
 		{/key}
-		<footer class="flex h-full items-center justify-center p-4 lg:p-8">
+		<footer class="flex h-full items-center justify-center gap-4 p-4 lg:p-8">
 			<p>&copy; Juanma 2025 🌱</p>
+			<a
+				href="https://www.digitalocean.com/?refcode=514208bfc1b8&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"
+				><img
+					src="https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%202.svg"
+					alt="DigitalOcean Referral Badge"
+				/></a
+			>
 		</footer>
 	</div>
 </ParaglideJS>
 
 <style lang="postcss">
-@keyframes fade-in {
-	from {
-		opacity: 0;
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
 	}
-}
 
-@keyframes fade-out {
-	to {
-		opacity: 0;
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
 	}
-}
 
-@keyframes slide-from-right {
-	from {
-		transform: translateX(30px);
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
 	}
-}
 
-@keyframes slide-to-left {
-	to {
-		transform: translateX(-30px);
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
 	}
-}
 
-:root::view-transition-old(root) {
-	animation:
-		90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
-		300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
-}
+	:root::view-transition-old(root) {
+		animation:
+			90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
 
-:root::view-transition-new(root) {
-	animation:
-		210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
-		300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
-}
+	:root::view-transition-new(root) {
+		animation:
+			210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+	}
 </style>
