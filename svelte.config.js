200d1c1e712d8ebed6f508965104c00ced825521
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2025-06-19 21:47:41 -0500

    Increased adapter-node limit for payloads

--- svelte.config.js
+++ svelte.config.js
@@ -37,11 +37,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			out: 'build'
+			out: 'build',
+			bodySizeLimit: 20 * 1024 * 1024 // 20 MB
 		}),
 		alias: {
 			$ui: './lib/ui',
-			$states: './src/states',
+			$states: './src/states'
 		}
 	}
 };
