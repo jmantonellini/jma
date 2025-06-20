fa05e0930309882b7bd6af24893a194d364121a8
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2025-06-19 21:34:32 -0500

    Fixed s3Client instance 2

--- src/lib/server/aws.ts
+++ src/lib/server/aws.ts
@@ -1,19 +1,16 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
-const region = process.env.AWS_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-if (!region || !accessKeyId || !secretAccessKey) {
-	throw new Error('Missing AWS credentials in environment variables');
-}
+let s3Client: S3Client | null = null;
 
 export function getS3Client() {
-	return new S3Client({
-		region: process.env.AWS_REGION!,
-		credentials: {
-			accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-		}
-	});
+	if (!s3Client) {
+		s3Client = new S3Client({
+			region: process.env.AWS_REGION,
+			credentials: {
+				accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+			}
+		});
+	}
+	return s3Client;
 }
