c6343de7079f12b960a97c47f8149427cde536c0
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2025-06-20 09:33:00 -0500

    Fixed wrong payloadLimit config

--- svelte.config.js
+++ svelte.config.js
@@ -37,8 +37,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			out: 'build',
-			bodySizeLimit: 20 * 1024 * 1024 // 20 MB
+			out: 'build'
 		}),
 		alias: {
 			$ui: './lib/ui',
